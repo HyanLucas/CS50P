bb6ff18174296bc2b808f8e02ae3a3bfd55b3f92
HyanLucas <hyanlucas2011@gmail.com>
2023-07-12 11:51:40 -0300

    feat: add coke.py

---
+++ Loops/coke.py
@@ -0,0 +1,20 @@
+def main():
+    print("Amount Due: 50")
+    coin()
+
+
+def coin():
+    coke_value = int(50)
+    while True:
+        coin_value = (int(input("Insert Coin: ")))
+        if coin_value == 25 or coin_value == 10 or coin_value == 5:
+            if (coke_value >= coin_value):
+                coke_value -= coin_value
+        if coke_value > 0:
+            print(f"Amount Due: {coke_value}")
+        else:
+            print(f"Change Owed: {coke_value * (-1)}")
+            break
+
+
+main()
