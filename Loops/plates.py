d1646954835cbe41fd0fe78303ef093f905cdf86
HyanLucas <hyanlucas2011@gmail.com>
2023-07-12 16:37:30 -0300

    feat: add plates.py

---
+++ Loops/plates.py
@@ -0,0 +1,34 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(plate):
+    if len(plate) <= 6 and len(plate) > 2:
+        if (plate[0].isalpha() and plate[1].isalpha()):
+            cont_number = 0
+            zero_first = False
+            letter_after_number = False
+            for number in plate:
+                if number == "0" and cont_number == 0:
+                    zero_first = True
+                if number.isdigit():
+                    cont_number += 1
+                if number.isalpha() and cont_number > 0:
+                    letter_after_number = True
+            if zero_first or letter_after_number:
+                return False
+            elif "." in plate or "," in plate or " " in plate or "'" in plate:
+                return False
+            else:
+                return True
+        else:
+            return False
+    else:
+        return False
+
+
+main()
