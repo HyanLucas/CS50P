c34abc91492c690218eb658054ff3ec81f509e82
HyanLucas <hyanlucas2011@gmail.com>
2023-07-12 11:21:10 -0300

    feat: add camel.py

---
+++ Loops/camel.py
@@ -0,0 +1,17 @@
+def main():
+    camelCase = str(input("camelCase: "))
+    snake_case_maker(camelCase)
+
+
+def snake_case_maker(camelCase):
+    snake_case = ""
+    for letter in camelCase:
+        if (letter.isupper()):
+            if (len(snake_case) > 0):
+                snake_case += "_" + letter.lower()
+        else:
+            snake_case += letter
+    return print(snake_case)
+
+
+main()
