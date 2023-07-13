b7aeb18b5b84c639b3c871acda8786f6e9d479b2
HyanLucas <hyanlucas2011@gmail.com>
2023-07-13 09:40:01 -0300

    feat: add fuel.py

---
+++ Exceptions/fuel.py
@@ -0,0 +1,33 @@
+def main():
+    gauge_calculate()
+
+
+def gauge_calculate():
+    values = []
+    while True:
+        gauge = str(input("Fraction: "))
+        try:
+            value1, value2 = gauge.split("/")
+            value1, value2 = int(value1), int(value2)
+        except (ValueError):
+            pass
+        if value1 > value2:
+            pass
+        else:
+            break
+    if value2 != 0:
+        if value1 != 0:
+            result = float(value1 / value2)
+            if value1 == value2:
+                return print("F")
+            elif result == 0.25:
+                return print("25%")
+            elif result == 0.50:
+                return print("50%")
+            elif result == 0.75:
+                return print("75%")
+        else:
+            return print("E")
+
+
+main()
