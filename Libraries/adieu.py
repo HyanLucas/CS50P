a80e33f8c752501af328c5805cde99454e62d183
HyanLucas <hyanlucas2011@gmail.com>
2023-07-27 09:34:13 -0300

    feat: add adieu.py

---
+++ Libraries/adieu.py
@@ -0,0 +1,17 @@
+import inflect
+
+names = []
+ift = inflect.engine()
+while True:
+    try:
+        answer = input("Name: ")
+        names.append(answer)
+    except EOFError:
+        if len(names) > 1:
+            last_name = names[-1]
+            names = ", ".join(names[0:-1])
+            print("Adieu, adieu, to", names, "and", last_name)
+            break
+        if len(names) == 1:
+            print("Adieu, adieu, to", names)
+            break
